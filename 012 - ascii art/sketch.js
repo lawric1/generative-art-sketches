2e6acf3ff9be055e429f7aaeeacd659c3f883f66
lawric1 <67882089+lawric1@users.noreply.github.com>
2023-01-22 12:34:00 -0300

    Add sketch 12

---
+++ 012 - ascii art/sketch.js
@@ -0,0 +1,80 @@
+let asciiPalette = [
+	"@",
+	"#",
+	"$",
+	"%",
+	"&",
+	"S",
+	"s",
+	"o",
+	"O",
+	"0",
+	"Q",
+	"D",
+	"d",
+	"C",
+	"c",
+	"Z",
+	"z",
+	"x",
+	"X",
+	"v",
+	"V",
+	">",
+	"<",
+	"+",
+	"-",
+	"=",
+	"^",
+	"~",
+	"*",
+	".",
+	",",
+	";",
+	":",
+	" ",
+];
+
+function preload() {
+	// img = loadImage("./image.jpg");
+	// img = loadImage("./image2.jpg");
+	img = loadImage("./image3.jpg");
+}
+
+function setup() {
+	createCanvas(img.width, img.height);
+
+	background(0);
+	fill(255);
+	stroke(100);
+
+	let tiles = 70;
+	let tileWidth = width / tiles;
+	let tileHeight = height / tiles;
+
+	textSize(tileWidth, tileHeight);
+
+	for (let row = 0; row < tiles; row++) {
+		for (let col = 0; col < tiles; col++) {
+			let x = col * tileWidth;
+			let y = row * tileHeight;
+
+			let pixel = img.get(x, y);
+
+			// Sum RGB values and divide by 3 to get pixel luminance
+			let luma = (pixel[0] + pixel[1] + pixel[2]) / 3;
+
+			let index = floor(map(luma, 0, 255, 0, asciiPalette.length - 1));
+			// Ascii palette is sorted from big to small
+			// invert index so darker colors use smaller ascii characters
+			index = asciiPalette.length - index;
+			let ascii = asciiPalette[index];
+
+			text(ascii, x, y);
+		}
+	}
+}
+
+// function mouseClicked() {
+// 	save("image.jpg");
+// }
